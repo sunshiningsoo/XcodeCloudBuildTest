49d171ce6d511b61ea0fd3a007a6eb07cc03a68e
sunshiningsoo <yenkee1219@gmail.com>
2022-11-20 14:50:59 +0900

    [Setting] 프로젝트 init

---
+++ XcodeCloudBuildTest/XcodeCloudBuildTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeCloudBuildTest
+//
+//  Created by 박성수 on 2022/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
